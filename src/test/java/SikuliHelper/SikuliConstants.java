b418076d655766a62244bdefb8f5137e26c4f2a0
sashipuhan <sashi.puhan4543@gmail.com>
2019-05-07 12:19:55 +0530

    Updated images in sikuli folder

--- src/test/java/SikuliHelper/SikuliConstants.java
+++ src/test/java/SikuliHelper/SikuliConstants.java
@@ -11,6 +11,139 @@ public class SikuliConstants {
 	public static final String phoneicon="src/test/resources/sikuliImages/phoneicon.PNG";
 	public static final String Settings="src/test/resources/sikuliImages/Settings.PNG";
 	public static final String closeicon="src/test/resources/sikuliImages/closeicon.PNG";
+	public static final String CALL_HISTORY="src/test/resources/sikuliImages/Callhistory.PNG";
+	public static final String BACK_ICON="src/test/resources/sikuliImages/BackIcon.PNG";
+	public static final String BATTERY_ICON="src/test/resources/sikuliImages/BatteryIcon.PNG";
+	public static final String CALL_ICON="src/test/resources/sikuliImages/CallIcon.PNG";
+	public static final String CONTACT_ICON="src/test/resources/sikuliImages/ContactIcon.PNG";
+	public static final String BLOCK_ICON="src/test/resources/sikuliImages/BlockText.PNG";
+	public static final String CANCEL_OPTION="src/test/resources/sikuliImages/CancelOption.PNG";
+	public static final String ADD_ICON="src/test/resources/sikuliImages/AddIcon.PNG";
+	public static final String ACCESSIBILITY_ICON="src/test/resources/sikuliImages/AccessibilityIcon.PNG";
+	public static final String DISPLAY_ICON="src/test/resources/sikuliImages/DisplayIcon.PNG";
+	public static final String APPNOTI_ICON="src/test/resources/sikuliImages/Apps&NotificationsIcon.PNG";
+	public static final String CONNECTDEVICE_ICON="src/test/resources/sikuliImages/ConnectedDevicesIcon.PNG";
+	public static final String GOOGLE_ICON="src/test/resources/sikuliImages/GoogleIcon.PNG";
+	public static final String USBPOWERSAVE_ICON="src/test/resources/sikuliImages/USBPowerSaveIcon.PNG";
+	public static final String STORAGE_ICON="src/test/resources/sikuliImages/StorageIcon.PNG";
+	public static final String SECURITYLOCA_ICON="src/test/resources/sikuliImages/Security&LocationIcon.PNG";
+	public static final String ACCESSORIES_ICON="src/test/resources/sikuliImages/Accessories Icon.PNG";
+	public static final String ACCESSORIES="src/test/resources/sikuliImages/Accessories.PNG";
+	public static final String AIRPLANEMODE_ICON="src/test/resources/sikuliImages/AirplaneModeIcon.PNG";
+	public static final String ADDBLOCK_NO="src/test/resources/sikuliImages/Addnoinblock.PNG";
+	public static final String ABOUT_TXT="src/test/resources/sikuliImages/AboutTxt.PNG";
+	public static final String ACCESSIBILITY_TXT="src/test/resources/sikuliImages/Accessibilitytxt.PNG";
+	public static final String APPIUMSETTINGS_ICON="src/test/resources/sikuliImages/AppiumSettings.PNG";
+	public static final String CALCULATOR_ICON="src/test/resources/sikuliImages/Calculator.PNG";
+	public static final String CALENDAR_ICON="src/test/resources/sikuliImages/Calendar.PNG";
+	public static final String CAMERA_ICON="src/test/resources/sikuliImages/Camera.PNG";
+	public static final String CHROME_ICON="src/test/resources/sikuliImages/Chrome.PNG";
+	public static final String CLOCK_ICON="src/test/resources/sikuliImages/Clock.PNG";
+	public static final String CONTACT_ICON2="src/test/resources/sikuliImages/ContactIcon2.PNG";
+	public static final String DOWNLOADS_ICON="src/test/resources/sikuliImages/Downloads.PNG";
+	public static final String DRIVE_ICON="src/test/resources/sikuliImages/Drive.PNG";
+	public static final String DUO_ICON="src/test/resources/sikuliImages/Duo.PNG";
+	public static final String FILEMANAGER_ICON="src/test/resources/sikuliImages/FileManager.PNG";
+	public static final String FMRADIO_ICON="src/test/resources/sikuliImages/FM Radio.PNG";
+	public static final String GMAIL_ICON="src/test/resources/sikuliImages/Gmail.PNG";
+	public static final String GOOGLEMENU_ICON="src/test/resources/sikuliImages/Googlemenu.PNG";
+	public static final String KEEPNOTES_ICON="src/test/resources/sikuliImages/KeepNotes.PNG";
+	public static final String MAP_ICON="src/test/resources/sikuliImages/Maps.PNG";
+	public static final String MESSAGING_ICON="src/test/resources/sikuliImages/Messaging.PNG";
+	public static final String PHONE_ICON="src/test/resources/sikuliImages/Phone.PNG";
+	public static final String PHOTOS_ICON="src/test/resources/sikuliImages/Photos.PNG";
+	public static final String PLAYMOVIE_ICON="src/test/resources/sikuliImages/PlayMovie.PNG";
+	public static final String PLAYMUSIC_ICON="src/test/resources/sikuliImages/PlayMusic.PNG";
+	public static final String PLAYSTORE_ICON="src/test/resources/sikuliImages/PlayStore.PNG";
+	public static final String QTILOGKIT_ICON="src/test/resources/sikuliImages/QTI Logkit.PNG";
+	public static final String SECURITY_ICON="src/test/resources/sikuliImages/Security.PNG";
+	public static final String SETTINGS_ICON="src/test/resources/sikuliImages/Settings.PNG";
+	public static final String SCOUT_ICON="src/test/resources/sikuliImages/Scout.PNG";
+	public static final String SOUNDRECORDER_ICON="src/test/resources/sikuliImages/SoundRecorder.PNG";
+	public static final String UNLOCKANDROID_ICON="src/test/resources/sikuliImages/Unlock.PNG";
+	public static final String VOICEACTIVATION_ICON="src/test/resources/sikuliImages/VoiceActivation.PNG";
+	public static final String VYSOR_ICON="src/test/resources/sikuliImages/Vysor.PNG";
+	public static final String WIPOWERAG="src/test/resources/sikuliImages/WiPowerAg....PNG";
+	public static final String YOUTUBE_ICON="src/test/resources/sikuliImages/YouTube.PNG";
+	public static final String NONE_TXT="src/test/resources/sikuliImages/Nonetext.PNG";
+	public static final String TOGGLE_ICON="src/test/resources/sikuliImages/toggle button.PNG";
+	public static final String GOOGLE_TXT="src/test/resources/sikuliImages/GoogleText.PNG";
+	public static final String RECENT_ICON="src/test/resources/sikuliImages/Recent icon.PNG";
+	public static final String DOWNLOADS_TXT="src/test/resources/sikuliImages/DownloadsTxt.PNG";
+	public static final String BLOCK_TXT="src/test/resources/sikuliImages/BlockText.PNG";
+	public static final String OK_TXT="src/test/resources/sikuliImages/OKtxt.PNG";
+	public static final String CANCEL_TXT="src/test/resources/sikuliImages/CancelTxt.PNG";
+	public static final String CONNECT_TXT="src/test/resources/sikuliImages/ConnectTxt.PNG";
+	public static final String ADDNETWORK_TXT="src/test/resources/sikuliImages/AddnetworkTxt.PNG";
+	public static final String MENUDOWN_ARROW="src/test/resources/sikuliImages/Menudownarrow.PNG";
+	public static final String OFF_TXT="src/test/resources/sikuliImages/Offtxt.PNG";
+	public static final String ON_TXT="src/test/resources/sikuliImages/Ontxt.PNG";
+	public static final String UNLOCK_ICON="src/test/resources/sikuliImages/UNLockicon.PNG";
+	public static final String WIFIDIRECT_TXT="src/test/resources/sikuliImages/Wi-Fi Direct.PNG";
+	public static final String WIFIPREFERENCES_TXT="src/test/resources/sikuliImages/Wi-Fi preferences.PNG";
+	public static final String WPSPUSH_TXT="src/test/resources/sikuliImages/WPSPushButton.PNG";
+	public static final String SHOWPASSWORD_TXT="src/test/resources/sikuliImages/Showpassword.PNG";
+	public static final String MANUAL_TXT="src/test/resources/sikuliImages/Manualtxt.PNG";
+	public static final String PROXYAUTOCONFIG_TXT="src/test/resources/sikuliImages/ProxyAutoConfig.PNG";
+	public static final String WPSPINENTRY_TXT="src/test/resources/sikuliImages/WPSPinEntry.PNG";
+	public static final String PROXY_TXT="src/test/resources/sikuliImages/Proxyoption.PNG";
+	public static final String ADVANCEDOPTION_TXT="src/test/resources/sikuliImages/Advancedoptions.PNG";
+	public static final String INSTALLCERTIFICATES_TXT="src/test/resources/sikuliImages/Install Certificates.PNG";
+	public static final String IPADDRESS_TXT="src/test/resources/sikuliImages/IP address.PNG";
+	public static final String MACADDRESS_TXT="src/test/resources/sikuliImages/MAC address.PNG";
+	public static final String NETWORKRATINGPROVIDER_TXT="src/test/resources/sikuliImages/Networkratingprovider.PNG";
+	public static final String OPENNETWORKNOTIFICATION_ICON="src/test/resources/sikuliImages/Opennetworknotificationicon.PNG";
+	public static final String SCANNINGSETTINGS_TXT="src/test/resources/sikuliImages/Scanning Settings.PNG";
+	public static final String WPSPUSH_ICON="src/test/resources/sikuliImages/WPSPushSymbol.PNG";
+	public static final String WPSPINENTRY_ICON="src/test/resources/sikuliImages/WPSPinEntrySymbol.PNG";
+	public static final String WPSPINENTRYMENU_ICON="src/test/resources/sikuliImages/WPSPinEntryMenu.PNG";
+	public static final String PEERDEVICES_ICON="src/test/resources/sikuliImages/PeerDevicesIcon.PNG";
+	public static final String PEERDEVICES_TXT="src/test/resources/sikuliImages/Peerdevices.PNG";
+	public static final String RENAMEDEVICE_TXT="src/test/resources/sikuliImages/Rename Device.PNG";
+	public static final String CALLENDTONE_TXT="src/test/resources/sikuliImages/Callendtone.PNG";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	
 	
