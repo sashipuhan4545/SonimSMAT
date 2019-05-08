1f73984a5bad11e2a1f6d7196f567778cd4e1d88
sashipuhan <sashi.puhan4543@gmail.com>
2019-05-08 11:26:57 +0530

    Added Sikuli constant

--- src/test/java/SikuliHelper/SikuliConstants.java
+++ src/test/java/SikuliHelper/SikuliConstants.java
@@ -101,6 +101,236 @@ public class SikuliConstants {
 	public static final String PEERDEVICES_TXT="src/test/resources/sikuliImages/Peerdevices.PNG";
 	public static final String RENAMEDEVICE_TXT="src/test/resources/sikuliImages/Rename Device.PNG";
 	public static final String CALLENDTONE_TXT="src/test/resources/sikuliImages/Callendtone.PNG";
+	public static final String FIRSTNAMEFIRST_TXT="src/test/resources/sikuliImages/NameformatFNF.PNG";
+	public static final String LASTNAMEFIRST_TXT="src/test/resources/sikuliImages/NameformatLNF.PNG";
+	public static final String VIBRATEFORCALLS_TXT="src/test/resources/sikuliImages/VibrateForCalls.PNG";
+	public static final String ADDRINGTONE_TXT="src/test/resources/sikuliImages/AddRingtone.PNG";
+	public static final String ALLOWONLYCONTACTS_TXT="src/test/resources/sikuliImages/allowonlycontacts.PNG";
+	public static final String ALLOWWHITELIST_TXT="src/test/resources/sikuliImages/Allowwhitelist.PNG";
+	public static final String ALWAYSFORWARD_TXT="src/test/resources/sikuliImages/Always forward.PNG";
+	//Call forwarding option always forward
+	public static final String PHONENO_TXT="src/test/resources/sikuliImages/PhoneNo.PNG";
+	public static final String DISABLE_ICON="src/test/resources/sikuliImages/APmodeDisable.PNG";
+	public static final String ENABLE_ICON="src/test/resources/sikuliImages/APmodeEnable.PNG";
+	public static final String ANDROIDA19E_TXT="src/test/resources/sikuliImages/Android_a19e.PNG";
+	public static final String SECURITYLOC_ICON="src/test/resources/sikuliImages/Security&LocationIcon.PNG";
+	public static final String SECURITYLOC_TXT="src/test/resources/sikuliImages/Security&Location.PNG";
+	public static final String BACK_TEXT="src/test/resources/sikuliImages/Back option.PNG";
+	public static final String BLOCKBLACKLIST_TXT="src/test/resources/sikuliImages/BlockBlackList.PNG";
+	public static final String CPNITPM_TXT="src/test/resources/sikuliImages/CPN-IT-PM.PNG";
+	public static final String COPYTOPHONE_TXT="src/test/resources/sikuliImages/CopytoPhone.PNG";
+	public static final String CREATESHORTCUT_TXT="src/test/resources/sikuliImages/Createshortcut.PNG";
+	public static final String ROUTETOVOICEMAIL_TXT="src/test/resources/sikuliImages/RoutetoVoicemail.PNG";
+	public static final String DELETE_TXT="src/test/resources/sikuliImages/Delete.PNG";
+	public static final String SETRINGTONE_TXT="src/test/resources/sikuliImages/Setringtone.PNG";
+	public static final String SHARE_TXT="src/test/resources/sikuliImages/Sharetxt.PNG";
+	public static final String CREATENEWCONTACT_TXT="src/test/resources/sikuliImages/Createnewcontact.PNG";
+	public static final String SETRINGTONE_TEXT="src/test/resources/sikuliImages/Setringtone.PNG";
+	public static final String CREATENEWCONTACT_ICON="src/test/resources/sikuliImages/CreatenewcontactIcon.PNG";
+	public static final String PREFIX_TXT="src/test/resources/sikuliImages/PrefixName.PNG";
+	public static final String LASTNAME_TXT="src/test/resources/sikuliImages/LastName.PNG";
+	public static final String MIDDLENAME_TXT="src/test/resources/sikuliImages/Middlename.PNG";
+	public static final String SUFFIX_TXT="src/test/resources/sikuliImages/Suffixname.PNG";
+	public static final String FIRSTNAME_TXT="src/test/resources/sikuliImages/Firstname.PNG";
+	public static final String EMAIL_TXT="src/test/resources/sikuliImages/Emailtxt.PNG";
+	public static final String MOBILE_TXT="src/test/resources/sikuliImages/Mobilecontact.PNG";
+	public static final String PHONE_TXT="src/test/resources/sikuliImages/PhoneText.PNG";
+	public static final String SAVE_TXT="src/test/resources/sikuliImages/Savetxt.PNG";
+	public static final String SEARCH_ICON="src/test/resources/sikuliImages/Searchicon.PNG";
+	public static final String VIDEOCALL_ICON="src/test/resources/sikuliImages/VideoCallicon.PNG";
+	public static final String DIALINGNO_ICON="src/test/resources/sikuliImages/Dialingnoicon.PNG";
+	public static final String VOICE_ICON="src/test/resources/sikuliImages/Voice icon.PNG";
+	public static final String FAVORITES_ICON="src/test/resources/sikuliImages/Favorites icon.PNG";
+	public static final String SEARCHCONTACTS_TXT="src/test/resources/sikuliImages/Search contacts.PNG";
+	public static final String SAVINGTOPHONE_TXT="src/test/resources/sikuliImages/Savingtophone.PNG";
+	public static final String SAVINGTOPHONE_ICON="src/test/resources/sikuliImages/Savingtophoneicon.PNG";
+	public static final String SIMCARD_TXT="src/test/resources/sikuliImages/SIMCardTxt.PNG";
+	public static final String CALLHISTORY_ICON="src/test/resources/sikuliImages/Callhistoryicon.PNG";
+	public static final String CONTACTS_ICON="src/test/resources/sikuliImages/Contacts icon.PNG";
+	public static final String MOREOPTIONS_ICON="src/test/resources/sikuliImages/Call settings Menu.PNG";
+	public static final String QUICKRESPONSES1_TXT="src/test/resources/sikuliImages/Quickresponses1.PNG";
+	//QUICKRESPONSES1----->Can't talk now.What's up?
+	public static final String QUICKRESPONSES2_TXT="src/test/resources/sikuliImages/Quickresponses2.PNG";
+	//QUICKRESPONSES1----->I'll call you right back
+	public static final String QUICKRESPONSES3_TXT="src/test/resources/sikuliImages/Quickresponses3.PNG";
+	//QUICKRESPONSES1----->I'll call you later
+	public static final String QUICKRESPONSES4_TXT="src/test/resources/sikuliImages/Quickresponses4.PNG";
+	//QUICKRESPONSES1----->Can't talk now.Call me later?
+	public static final String SIPACCOUNTS_TXT="src/test/resources/sikuliImages/SIP accounts.PNG";
+	public static final String TERMSOFSERVICE_TXT="src/test/resources/sikuliImages/Terms of service.PNG";
+	public static final String ONLYFORSIPCALLS_TXT="src/test/resources/sikuliImages/OnlyforSIPcalls.PNG";
+	public static final String SIPFORALLCALLS_TXT="src/test/resources/sikuliImages/SIPforallcalls.PNG";
+	public static final String WHENUNANSWERED_TXT="src/test/resources/sikuliImages/When unanswered.PNG";
+	//Call forwarding when unanswered
+	public static final String WHENUNREACHABLE_TXT="src/test/resources/sikuliImages/When unreachable.PNG";
+	//Call forwarding when unreachable
+	public static final String WHENBUSY_TXT="src/test/resources/sikuliImages/When busy.PNG";
+	//Call forwarding when busy
+	public static final String DISPLAYOPTION_TXT="src/test/resources/sikuliImages/Display options.PNG";
+	public static final String SOUNDSANDVIBRATION_TXT="src/test/resources/sikuliImages/Soundsandvibration.PNG";
+	public static final String QUICKRESPONSES_TXT="src/test/resources/sikuliImages/Quick responses.PNG";
+	public static final String SPEEDDIALSETTING_TXT="src/test/resources/sikuliImages/Speed dial settings.PNG";
+	public static final String CALLS_TXT="src/test/resources/sikuliImages/Calls.PNG";
+	public static final String CALLSCREENING_TXT="src/test/resources/sikuliImages/Call Screening.PNG";
+	public static final String VOICEMAIL_TXT="src/test/resources/sikuliImages/Voicemail.PNG";
+	public static final String SORTBY_TXT="src/test/resources/sikuliImages/Sort by.PNG";
+	public static final String NAMEFORMAT_TXT="src/test/resources/sikuliImages/Nameformat.PNG";
+	//SOUNDS AND VIBRATION
+	public static final String PHONERINGTONE_TXT="src/test/resources/sikuliImages/Phone Ringtone.PNG";
+    public static final String DIALPADTONES_TXT="src/test/resources/sikuliImages/Dialpadtones.PNG";
+    //CALL SETTINGS
+	public static final String CALLINGACCOUNTS_TXT="src/test/resources/sikuliImages/CallingAccount.PNG";
+	public static final String FDN_TXT="src/test/resources/sikuliImages/FDN.PNG";
+	//FDN------------------>FIXED DIALING NO
+	public static final String IMSSETTINGS_TXT="src/test/resources/sikuliImages/IMS SETTINGS.PNG";
+	public static final String ADDITIONALSETTINGS_TXT="src/test/resources/sikuliImages/AdditionalSettings.PNG";
+	public static final String ATTACH_ICON="src/test/resources/sikuliImages/Attachicon.PNG";
+	public static final String GROUPNOIFICATION_TXT="src/test/resources/sikuliImages/Groupnotification.PNG";
+	//MESSAGE GROUP NOTIFICATION TEXT
+	public static final String MARKALLASREAD_TXT="src/test/resources/sikuliImages/Markallasread.PNG";
+	public static final String MESSAGETO_TXT="src/test/resources/sikuliImages/MessageToOption.PNG";
+	public static final String ENABLEGESTURE_TXT="src/test/resources/sikuliImages/EnableGesture.PNG";
+	public static final String MOREMENU_ICON="src/test/resources/sikuliImages/Menu.PNG";
+	public static final String NOTIFICATIONS_TXT="src/test/resources/sikuliImages/Notification.PNG";
+	public static final String MESSAGECONTACT_ICON="src/test/resources/sikuliImages/MessageContact.PNG";
+	//MESSAGE SELECT CONTACT OPTION
+	public static final String SOUND_TXT="src/test/resources/sikuliImages/Soundtxt.PNG";
+	public static final String TO_TXT="src/test/resources/sikuliImages/Totxt.PNG";
+	public static final String CREATENEWMSG_ICON="src/test/resources/sikuliImages/Create new msg.PNG";
+	public static final String RESTOREDEFAULTSETTINGS_TXT="src/test/resources/sikuliImages/Default Settingsmessage.PNG";
+	public static final String CHANGESMSAPP_TXT="src/test/resources/sikuliImages/Changesmsapp.PNG";
+	public static final String CHECKBOX_ICON="src/test/resources/sikuliImages/checkbox.PNG";
+	public static final String PASSWORDTYPING_TXT="src/test/resources/sikuliImages/Passwordtyping.PNG";
+	public static final String WIFICONNECTION_ICON="src/test/resources/sikuliImages/WiFiUNicon.PNG";
+	public static final String UPARROW_ICON="src/test/resources/sikuliImages/Uparrow.PNG";
+	public static final String CHOOSEPHOTO_TXT="src/test/resources/sikuliImages/ChoosePhoto.PNG";
+	public static final String TAKEPHOTO_TXT="src/test/resources/sikuliImages/TakePhoto.PNG";
+	public static final String CLEARNO_ICON="src/test/resources/sikuliImages/Clearnoicon.PNG";
+	public static final String EDITQUICKRESPONSE_TXT="src/test/resources/sikuliImages/EditQuickresponse.PNG";
+	public static final String HOTSPOTTETHER_TXT="src/test/resources/sikuliImages/Hotspot&tethering.PNG";
+	public static final String HOTSPOTTETHER_ICON="src/test/resources/sikuliImages/Hotspot&tethering Icon.PNG";
+	public static final String ALARMSMENUDOWN_ICON="src/test/resources/sikuliImages/Alarmsmenudown.PNG";
+	public static final String ALARMMORE_ICON="src/test/resources/sikuliImages/Alarms Menu.PNG";
+	public static final String ALARMTOOL1_ICON="src/test/resources/sikuliImages/Alarmstoolbar.PNG";
+	public static final String ALARMTOOL2_ICON="src/test/resources/sikuliImages/Alarmstoolbar2.PNG";
+	public static final String SIM_ICON="src/test/resources/sikuliImages/SIMcard.PNG";
+	public static final String SIMCARDS_TXT="src/test/resources/sikuliImages/SIMtxt.PNG";
+	public static final String RESETWIFI="src/test/resources/sikuliImages/ResetWIFIMobileBT.PNG";
+	//RESET WIFI,MOBILE,BLUETOOTH
+	public static final String RESET_ICON="src/test/resources/sikuliImages/ResetIcon.PNG";
+	public static final String VPN_ICON="src/test/resources/sikuliImages/VPNIcon.PNG";
+	public static final String VPN_TXT="src/test/resources/sikuliImages/VPN.PNG";
+	public static final String DATAUSAGE_ICON="src/test/resources/sikuliImages/DatausageIcon.PNG";
+	public static final String DATAUSAGE_TXT="src/test/resources/sikuliImages/Data usage.PNG";
+	public static final String ETHERNETSET_TXT="src/test/resources/sikuliImages/Ethernet Settings.PNG";
+	public static final String ICMENU_ICON="src/test/resources/sikuliImages/ICmenu.PNG";
+	public static final String MODIFIED_TXT="src/test/resources/sikuliImages/Modified.PNG";
+	public static final String TOOLBAR1_ICON="src/test/resources/sikuliImages/ToolbarIcon.PNG";
+	public static final String TOOLBAR2_ICON="src/test/resources/sikuliImages/ToolbarIcon2.PNG";
+	public static final String NOITEMS_TXT="src/test/resources/sikuliImages/Noitemstxt.PNG";
+	public static final String NAMEOPTION_TXT="src/test/resources/sikuliImages/Nameoption.PNG";
+	public static final String XP8800_ICON="src/test/resources/sikuliImages/XP8800 Icon.PNG";
+	public static final String XP8800_TXT="src/test/resources/sikuliImages/XP8800.PNG";
+	public static final String DRIVE_TXT="src/test/resources/sikuliImages/Driveoption.PNG";
+	public static final String RECENT_TXT="src/test/resources/sikuliImages/Recent.PNG";
+	public static final String SOUNDRECORDER_TXT ="src/test/resources/sikuliImages/SoundRecorder.PNG";
+	public static final String DOWNLOAD_TXT="src/test/resources/sikuliImages/Download.PNG";
+	public static final String DATA_TXT="src/test/resources/sikuliImages/Data.PNG";
+	public static final String RENAMETYPE_TXT="src/test/resources/sikuliImages/ToolbarIcon.PNG";
+	public static final String SEARCHFORDEVICE_TXT="src/test/resources/sikuliImages/SearchForDevices.PNG";
+	public static final String QREDIT_TXT="src/test/resources/sikuliImages/QREdit.PNG";
+	public static final String MOBILENETWORK_ICON="src/test/resources/sikuliImages/MobilenetworkIcon.PNG";
+	public static final String MOBILENETWORK_TXT="src/test/resources/sikuliImages/Mobilenetwork.PNG";
+	public static final String SYSTEM_ICON="src/test/resources/sikuliImages/SystemIcon.PNG";
+	public static final String USBPS_ICON="src/test/resources/sikuliImages/USBPowerSaveIcon.PNG";
+	public static final String USBPS_TXT="src/test/resources/sikuliImages/USBPowerSaving.PNG";
+	//UA-------->USERS&ACCOUNTS
+	public static final String UA_ICON="src/test/resources/sikuliImages/Users&accountsIcon.PNG";
+	public static final String UA_TXT="src/test/resources/sikuliImages/Users&accounts.PNG";
+	//PROGRAMMABLE KEYS
+	public static final String PK_ICON="src/test/resources/sikuliImages/ProgrammableKeysIcon.PNG";
+	public static final String PK_TXT="src/test/resources/sikuliImages/Programmable Keys.PNG";
+	//APN------>ACCESS POINT NAMES
+	public static final String APN_TXT="src/test/resources/sikuliImages/AccessPointNames.PNG";
+	public static final String AUTOMATSELECTNW_TXT="src/test/resources/sikuliImages/Automaticallyselectnetwork.PNG";
+	public static final String SIMSLOT2_TXT="src/test/resources/sikuliImages/SIMSLOT2.PNG";
+	//PREFERRED NETWORK TYPE----->PNT
+	public static final String PNTLTE_TXT="src/test/resources/sikuliImages/LTE.PNG";
+	public static final String PNT2G_TXT="src/test/resources/sikuliImages/2G.PNG";
+	public static final String PNT3G_TXT="src/test/resources/sikuliImages/3G.PNG";
+	public static final String MOBILEDATA_TXT="src/test/resources/sikuliImages/Mobiledata.PNG";
+	public static final String ADVANCED_TXT="src/test/resources/sikuliImages/Advanced.PNG";
+	public static final String ROAMING_TXT="src/test/resources/sikuliImages/LTE.PNG";
+	//SIC------->SCREENING INCOMING CALL
+	public static final String SIC_TXT="src/test/resources/sikuliImages/Incomingcall.PNG";
+	public static final String SICSETTING_TXT="src/test/resources/sikuliImages/SICsetting.PNG";
+	
+	//SOC------->SCREENING OUTGOING CALL
+	public static final String SOC_TXT="src/test/resources/sikuliImages/Outgoingcal.PNG";
+	public static final String WLADDNO_TXT="src/test/resources/sikuliImages/WLaddno.PNG";
+	public static final String USB_ICON="src/test/resources/sikuliImages/USB Icon.PNG";
+	public static final String YES_TXT="src/test/resources/sikuliImages/YES.PNG";
+	public static final String NO_TXT="src/test/resources/sikuliImages/NO.PNG";
+	public static final String PAIR_TXT="src/test/resources/sikuliImages/PAIR.PNG";
+	public static final String CAST_ICON="src/test/resources/sikuliImages/Casticon.PNG";
+	public static final String NFC_ICON="src/test/resources/sikuliImages/NFC icon.PNG";
+	public static final String NFC_TXT="src/test/resources/sikuliImages/NFC.PNG";
+	public static final String CARKIT_TXT="src/test/resources/sikuliImages/CarKitautopair.PNG";
+	public static final String CHROMEBOOK_ICON="src/test/resources/sikuliImages/Chromebook icon.PNG";
+	public static final String ADD_TXT="src/test/resources/sikuliImages/Addtxt.PNG";
+	public static final String RENAME_TXT="src/test/resources/sikuliImages/Renametxt.PNG";
+	public static final String PRINT_ICON="src/test/resources/sikuliImages/Printing icon.PNG";
+	public static final String SAVING2PHONE_TXT="src/test/resources/sikuliImages/Savingtophone.PNG";
+	public static final String SAVING2PHONE_ICON="src/test/resources/sikuliImages/Savingtophoneicon.PNG";
+	public static final String S2PHONE_TXT="src/test/resources/sikuliImages/S2phone.PNG";
+	public static final String CAMERACONTACT_ICON="src/test/resources/sikuliImages/Cameracontacticon.PNG";
+	public static final String TRANSFERFILES_TXT="src/test/resources/sikuliImages/Transfer files.PNG";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
